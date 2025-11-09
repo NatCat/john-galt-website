d55a6c2238bed142cafab9ce683aaec794aa6800
NatCat <sella.haze@gmail.com>
2025-11-09 20:16:56 +0100

    astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig } from 'astro/config';
 import path from 'path';
 
-// https://astro.build/config
+// replace <user> and <repo> with your GitHub info
 export default defineConfig({
+  site: 'https://NatCat.github.io/john-galt-website',    // omit base if this is a user/organization site
+  base: '/john-galt-website/',
+
   vite: {
     resolve: {
       alias: {
-        'netlify-identity-widget': path.resolve('node_modules/netlify-identity-widget/build/netlify-identity-widget.js')
-      }
-    }
-  }
+        'netlify-identity-widget': path.resolve(
+          'node_modules/netlify-identity-widget/build/netlify-identity-widget.js'
+        ),
+      },
+    },
+  },
 });
